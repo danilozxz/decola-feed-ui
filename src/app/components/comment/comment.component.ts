e7aaaa87c8d8b4299e539dca7ddf25d2cb972a12
Danilozxz <progdanilo@gmail.com>
2025-03-31 17:38:23 -0300

    feat: add método para deletar comentários

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentService } from '../../services/comment/comment.service';
 
 @Component({
   selector: 'app-comment',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.css'
 })
-export class CommentComponent{
+export class CommentComponent {
   @Input() comments: any[] = [];
 
   constructor(private commentService: CommentService) {}
 
+  deleteComment(id: number) {
+    this.commentService.deleteComment(id).subscribe(() => {
+      this.comments = this.comments.filter(comment => comment.id !== id);
+    });
+  }
 }
--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -25,4 +25,8 @@ export class CommentService {
     return this.http.post<any>(`${this.apiUrl}/add`, comment)
   }
 
+  deleteComment(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
 }
